7830c8133d2a36495fca0a51deed1659e63f548d
IzLeandro <34462612+IzLeandro@users.noreply.github.com>
2020-11-13 19:52:21 -0600

    mucha cosa

--- funciones.py
+++ funciones.py
@@ -1,5 +1,5 @@
 from archivos import grabar,leer
-
+import re
 def GuardarDiccionario(dic):
     pregunta=input("Ingrese el nombre con el que desea guardar el archivo: ")
     grabar(pregunta,dic)
@@ -19,9 +19,27 @@ def cargarDiccionario():
             print("Ingrese un valor correcto.")
 #! FALTA COMPARAR NUMERO DE TELEFONO CONTRA REGEX
 def ingresar(dic):
-        numero=input("digite el número de teléfono: ")
-        sucursal=input("Dijite la sucursal: ")
-        
+    while True:
+        codigo=revisarUltimoCodigo(dic) + 1
+        estado=["Por entregar","Entregado","Devuelto"]
+        numTelefono=input("digite el número de teléfono: ")
+        if not re.match("^\d{8}$",numTelefono):
+            print("Ingrese un numero de telefono correcto.")
+            continue
+        sucursal=input("Digite la sucursal: ")
+        preguntaEstado=input("1.Por entregar.\n2.Entregado.\n3.Devuelto.\nCual es el estado del paquete?: ")
+        try:
+            preguntaEstado=eval(preguntaEstado)
+            if preguntaEstado < 0 and preguntaEstado > 4:
+                print("Ingrese un valor entre 1 y 3.")
+                continue
+        except:
+            print("Ingrese solo números.")
+            continue
+        if preguntaEstado==1:
+            dic[codigo]=[numTelefono,sucursal,10,estado[preguntaEstado-1]]
+        else:
+            dic[codigo]=[numTelefono,sucursal,0,estado[preguntaEstado-1]]
         return dic
 
 def revisarUltimoCodigo(dic):
@@ -30,5 +48,36 @@ def revisarUltimoCodigo(dic):
         ultimo=i
     return ultimo
 
-dic={1:"a",2:"b",3:"c"}
+def actualizar(dic):
+    while True:
+        try: codigoABuscar=eval(input("digite el código del paquete: "))
+        except:
+            print("Solo se admiten números.")
+        for i in dic.keys():
+            if i == codigoABuscar:
+                preguntaEstado=input("1.Por entregar.\n2.Entregado.\n3.Devuelto.\nCual es el estado del paquete?: ")
+                try:
+                    preguntaEstado=eval(preguntaEstado)
+                    if preguntaEstado < 0 and preguntaEstado > 4:
+                        print("Ingrese un valor entre 1 y 3.")
+                        continue
+                except:
+                    print("Ingrese solo números.")
+                    continue
+                if preguntaEstado==1:
+                    dic[i][2]=5
+                else:
+                    dic[i][2]=0
+                return dic
+        else:
+            print("El código indicado no se encuentra en el registro.")
 
+def reporte(dic):
+    print("Total de paquetes registrados: ",revisarUltimoCodigo(dic))
+    for i in dic.keys():
+        print("Numero de paquete:",i)
+        print("Numero de Telefono:",dic[i][0])
+        print("Sucursal:",dic[i][1])
+        print("Cantidad de días hábiles:",dic[i][2])
+        print("Estado:",dic[i][3],"\n")   
+    return dic
--- wish.py
+++ wish.py
@@ -1,4 +1,4 @@
-from funciones import leer,grabar,GuardarDiccionario,cargarDiccionario
+from funciones import GuardarDiccionario,cargarDiccionario,ingresar,actualizar,reporte
 dic=cargarDiccionario()
 def menu():
     global dic
@@ -15,17 +15,22 @@ def menu():
 \n\n""")
         print("1.Ingresar un paquete.\n2.Actualizar estado del paquete.\n3.Eliminar paquete.\n4.Reportar paquete.\n5.Salir")
         opcion=input("Ingrese una opción: ")
+        print("")
         if opcion=="1":
-            print()
+            dic=ingresar(dic)
+            input("Se ha ingresado el dato, presione enter para continuar.")
         if opcion=="2":
-            print()
+            dic=actualizar(dic)
+            input("Se ha realizado el cambio, presione enter para continuar.")
         if opcion=="3":
             print()
         if opcion=="4":
-            print()
+            reporte(dic)
+            input("datos mostrados satisfactoriamente, presione enter para continuar.")
         if opcion=="5":
             GuardarDiccionario(dic)
             print("Gracias por usar nuestros servicios, hasta pronto.")
+            print(dic)
             return ""
         else:
             print("Ingrese un valor valido.")
